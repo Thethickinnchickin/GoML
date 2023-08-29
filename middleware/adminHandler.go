62f92a85c45c96f51eb0300252bb16f79e1d1af6
Thethickinnchickin <matt.reiley@sjsu.edu>
2023-08-29 15:16:46 -0700

    second commit

---
+++ middleware/adminHandler.go
@@ -0,0 +1,12 @@
+// middleware/adminHandler.go
+
+package middleware
+
+import (
+    "net/http"
+)
+
+func AdminHandler(w http.ResponseWriter, r *http.Request) {
+    // Your code for the /admin route handler goes here
+    // You can use w to write responses and r to read requests
+}
---
+++ middleware/protectedHandler.go
@@ -0,0 +1,12 @@
+// middleware/protectedHandler.go
+
+package middleware
+
+import (
+    "net/http"
+)
+
+func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
+    // Your code for the /protected route handler goes here
+    // You can use w to write responses and r to read requests
+}
